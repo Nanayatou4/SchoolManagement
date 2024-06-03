f0fc754a6dd46fd50165a3b34bd843bb4f197146
gorguimarena <gorguimarena@gmail.com>
2024-06-03 16:29:18 +0000

    Interface for the admin and start making interface to add edit and delete note

--- Controllers/ExamenController.js
+++ Controllers/ExamenController.js
@@ -1,22 +1,37 @@
+db = require('../Models/sequelize');
 module.exports= examenController= {
 
-    exam_add: function (req, res) {
-
+    add_exam_page: function (req, res) {
+        res.render("./add-exam_page.ejs");
     }
     ,
 
+    edit_exam_page: function (req, res) {
+        const id=req.params.id;
+        res.render("./edit-exam_page.ejs",{id});
+    }
+    ,
     edit_exam: function (req, res) {
-
+        const id = req.params.id;
+        db.Examen.findOne({where : { id: id} }).then(result=>{
+            res.render('', {result});
+        }).catch(err => {
+            res.status(500).send({ error: 'Something went wrong' });
+        });
     }
     ,
-
+    add_exam: function (req, res) {
+        res.render("./edit-exam_page.ejs");
+    },
     delete_exam: function (req, res) {
         const id = req.params.id;
-
     }
     ,
     exam_Page(req, res) {
-        res.render("./exam.ejs");
-    }
+        db.Examen.findAll().then(exams =>{
+            res.render("./exam.ejs", {exams});
+        });
+    },
+
 
 };
--- Routers/Routes.js
+++ Routers/Routes.js
@@ -4,6 +4,7 @@ const controllers_Compte=require("../Controllers/CompteController")
 const controllers_main=require("../Controllers/mainController")
 const exam_controller=require("../Controllers/ExamenController")
 
+
 const Route = express.Router();
 
 Route.get('/',(req,res)=>{
@@ -13,9 +14,12 @@ Route.get('/',(req,res)=>{
 Route.get('/register', controllers_student.register);
 Route.post('/login', controllers_Compte.login);
 Route.post('/enregistrer',controllers_main.save)
-Route.get('/', controllers_main.login);
+Route.get('/',controllers_main.login);
 Route.get('/password',controllers_main.pwd);
+Route.get('/exam',exam_controller.exam_Page);
 
-Route.get('/exam',exam_controller.exam_Page)
+Route.get('/add-exam',exam_controller.add_exam_page);
+Route.get('/edit-exam/:id',exam_controller.edit_exam_page);
+Route.get('/delete-exam/:id',exam_controller.delete_exam);
 
 module.exports=Route;
